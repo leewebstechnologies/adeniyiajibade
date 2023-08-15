74024faf4c88c3966fcf2c798803c7fc9c173c75
leewebstechnologies <aliuajibade317@gmail.com>
2023-08-15 17:26:04 +0100

    navbar added

---
+++ src/app/page.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
+
+
+
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+// export default function Home() {
+//   return <div>Home</div>;
+// }
