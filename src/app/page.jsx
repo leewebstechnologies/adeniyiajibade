24f8be94a26aa1cff55bd0dd3274a7a6f5716361
leewebstechnologies <aliuajibade317@gmail.com>
2023-08-16 22:41:13 +0100

    home page completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,30 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
 
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div className={styles.container}>Home</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
+    </div>
+  );
+};
 
-export default Home
-
-
+export default Home;
 
 // import Image from "next/image";
 // import styles from "./page.module.css";
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,31 +9,31 @@ const Footer = () => {
       <div className={styles.social}>
         <Image
           src="/1.png"
-          width={15}
-          height={15}
+          width={20}
+          height={20}
           className={styles.icon}
-          alt="Facebook Account"
+          alt="Lama Dev Facebook Account"
         />
         <Image
           src="/2.png"
-          width={15}
-          height={15}
+          width={20}
+          height={20}
           className={styles.icon}
-          alt="Instagram Account"
+          alt="Lama Dev"
         />
         <Image
           src="/3.png"
-          width={15}
-          height={15}
+          width={20}
+          height={20}
           className={styles.icon}
-          alt="Twitter Account"
+          alt="Lama Dev"
         />
         <Image
           src="/4.png"
-          width={15}
-          height={15}
+          width={20}
+          height={20}
           className={styles.icon}
-          alt="Youtube Account"
+          alt="Lama Dev"
         />
       </div>
     </div>
