0d3b757221e7e2df76b878e8dcb57ca03948e26d
leewebstechnologies <aliuajibade317@gmail.com>
2023-08-20 21:25:05 +0100

    portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,14 +7,14 @@ const Portfolio = () => {
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
-        <Link href="/portfolio/illustrations" className={styles.item}>
-          <span className={styles.title}>Illustrations</span>
+        <Link href="/portfolio/graphics" className={styles.item}>
+          <span className={styles.title}>Graphics</span>
         </Link>
         <Link href="/portfolio/websites" className={styles.item}>
           <span className={styles.title}>Websites</span>
         </Link>
         <Link href="/portfolio/applications" className={styles.item}>
-          <span className={styles.title}>Applications</span>
+          <span className={styles.title}>Web Apps</span>
         </Link>
       </div>
     </div>
